7b452ffea95d2cb60d3c451fe90833f1f9c65465
Mateus Felipe <mateus_f97@hotmail.com>
2022-08-26 09:02:10 -0300

    Animations

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $(".menu").click(function() {
         if(window.scrollY < 60){
-            $(".hidden-menu").toggle("slow");
+            $(".hidden-menu").slideToggle("slow");
         }
         window.scroll({top: 0, left: 0, behavior: 'smooth'});
     });
@@ -19,5 +19,30 @@ $(document).ready(function() {
             $(".menu").attr('src', 'images/hamburguer.png');
         }
     });
+
+    function idade(birth_year, birth_month, birth_day) {
+        var d = new Date,
+            current_year = d.getFullYear(),
+            current_month = d.getMonth() + 1,
+            current_day = d.getDate(),
+
+    
+            birth_year = +birth_year,
+            birth_month = +birth_month,
+            birth_day = +birth_day,
+    
+            age = current_year - birth_year;
+
+            $("#year").text(current_year);
+
+    
+        if (current_month < birth_month || current_month == birth_month && current_day < birth_day) {
+            age--;
+        }
+    
+        return age < 0 ? 0 : age;
+    }
+
+    $("#Age").text(idade(1997, 12, 15));
 
 });
