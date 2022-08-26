536b158267ae18edc07db31986a2ca5504299e72
Mateus Felipe <mateus_f97@hotmail.com>
2022-08-26 06:19:40 -0300

    Scroll and Hamburguer Menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
 
     $(".menu").click(function() {
-        $(".hidden-menu").toggle("slow");
+        if(window.scrollY < 60){
+            $(".hidden-menu").toggle("slow");
+        }
+        window.scroll({top: 0, left: 0, behavior: 'smooth'});
     });
 
     if ($(document).width() < 768) {
         $(".hidden-menu").toggle();
     }
+
+
+    window.addEventListener("scroll", function(){
+        if(window.scrollY > 60){
+            $(".menu").attr('src', 'images/up.png');
+        } else {
+            $(".menu").attr('src', 'images/hamburguer.png');
+        }
+    });
 
 });
